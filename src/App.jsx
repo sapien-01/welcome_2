ee0e499e2ec6280262a1ab65874280fd8cb0171f
sapien-01 <sapiennovel01@gmail.com>
2024-07-23 21:21:43 +0500

    changed message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello, World!</h1>
-      <p>Welcome to your Vite + React app.</p>
+      <p>Welcome to your Vite + React = app.</p>
     </div>
   );
 }
